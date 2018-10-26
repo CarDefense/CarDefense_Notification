93e1f6ae40b8d23eb68ba2ffabab518eb6029225
João Gabriel Rossi <bielrossiborba@gmail.com>
2018-10-26 13:15:54 -0300

    Updating ImageField maxsize

--- notifications/models.py
+++ notifications/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Notifications(models.Model):
-    token = models.CharField(max_length=50)
+    token = models.CharField(max_length=70)
     title = models.CharField(max_length=20)
     message = models.CharField(max_length=50)
-    image = models.CharField(null=True, blank=True, max_length=70)
+    image = models.CharField(null=True, blank=True, max_length=250)
 
 
 class EmergencyNotifications(models.Model):
     title = models.CharField(max_length=20)
     message = models.CharField(max_length=50)
-    image = models.CharField(null=True, blank=True, max_length=70)
+    image = models.CharField(null=True, blank=True, max_length=250)
 
 
 class NotificationsImage(models.Model):
