40925acdb826cad270ecec1f78208f2971037d55
tayh <tayhcarvalho@gmail.com>
2018-11-01 21:53:14 -0300

    Fixing prod script

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,3 +1,3 @@
-docker build -t tayh/api_notifications:homolog .
+docker build -t tayh/api_notifications:prod .
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push tayh/api_notifications:homolog
+docker push tayh/api_notifications:prod
