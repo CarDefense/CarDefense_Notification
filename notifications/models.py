40ed38f3c231b9d84c9aca4a51610f868c59d4a4
João Gabriel Rossi <bielrossiborba@gmail.com>
2018-11-07 01:10:39 -0200

    Renaming variable "token" and changing field

--- notifications/models.py
+++ notifications/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Notifications(models.Model):
-    token = models.CharField(max_length=70)
+    id_token = models.BigIntegerField()
     title = models.CharField(max_length=20)
     message = models.CharField(max_length=50)
     image = models.CharField(null=True, blank=True, max_length=250)
